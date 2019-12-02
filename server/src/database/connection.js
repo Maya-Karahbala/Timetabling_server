ab0d46a54865004143c51db948add607759fa57d
Unknown <maya.karabala8@gmail.com>
2019-12-02 03:46:17 +0300

    optional parameter for get opened courses

--- server/src/database/connection.js
+++ server/src/database/connection.js
@@ -1,14 +1,11 @@
 const Sequelize= require("sequelize")
-
-
 const sequelize=new Sequelize("test4","root","123",{
     host: "127.0.0.1",
     dialect: "mysql",
     define: {
         timestamps: false
     }
-    //operatorsAliases: false
 })
-//sequelize will be used to init all modules so we need to make it global
+//sequelize will be used to initilize all models so we need to make it global
 module.exports=sequelize
 global.sequelize=sequelize
